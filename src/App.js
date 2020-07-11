da4b8dee8f5ea596cb574a7b13d34f336829cec2
frankcs96 <fcsuarez96@gmail.com>
2020-07-11 19:38:36 +0200

    Bubblesort implemented TODO: autoAnimation

--- src/App.js
+++ src/App.js
@@ -3,22 +3,36 @@ import "./App.css";
 import Menu from "./components/menu/Menu";
 import Visualizer from "./components/visualizer/Visualizer";
 import generateRandomArray from "./utils/arraygenerator";
-import bubbleSort from "./algorithms/bubblesort";
+import bubbleSort, { bubbleSortAnimation } from "./algorithms/bubblesort";
 import debugArray from "./algorithms/debug";
 
 function App() {
-  let arraySize = 80;
+  let arraySize = 10;
 
   const [array, setArray] = useState(generateRandomArray(arraySize, 50, 280));
+  const [bubbleSortState, setBubbleSortState] = useState({ i: 0, j: 0 });
 
   const resetArray = () => {
     setArray(generateRandomArray(arraySize, 50, 280));
   };
 
   const sortArray = () => {
-    setArray(bubbleSort(array));
+    setArray(bubbleSort(array, bubbleSortState.i, bubbleSortState.j));
+
+    let bubbleSortPointers = bubbleSortAnimation(
+      array,
+      bubbleSortState.i,
+      bubbleSortState.j
+    );
+
+    setBubbleSortState({ i: bubbleSortPointers.i, j: bubbleSortPointers.j });
+
+    if (debugArray(array)) {
+      setBubbleSortState({ i: 0, j: 0 });
+    }
     //DEBUG: check if array is sorted
     console.log(debugArray(array));
+    console.log(bubbleSortState.i, bubbleSortState.j);
   };
 
   return (
--- src/algorithms/bubblesort.js
+++ src/algorithms/bubblesort.js
@@ -1,15 +1,24 @@
-export default function bubbleSort(array) {
+export default function bubbleSort(array, i, j) {
   const sortedArray = [...array];
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1 - i; j++) {
-      if (sortedArray[j] > sortedArray[j + 1]) {
-        let temp = sortedArray[j];
-        sortedArray[j] = sortedArray[j + 1];
-        sortedArray[j + 1] = temp;
-      }
-    }
+  if (sortedArray[j] > sortedArray[j + 1]) {
+    let temp = sortedArray[j];
+    sortedArray[j] = sortedArray[j + 1];
+    sortedArray[j + 1] = temp;
   }
 
   return sortedArray;
+}
+
+export function bubbleSortAnimation(array, i, j) {
+  if (i < array.length) {
+    if (j < array.length - 1 - i) {
+      j++;
+    } else {
+      j = 0;
+      i++;
+    }
+  }
+
+  return { i, j };
 }
