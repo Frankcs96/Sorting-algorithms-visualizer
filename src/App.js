62a2fe2d4331ab8a173406f843aab54ae584b0a0
frankcs96 <fcsuarez96@gmail.com>
2020-07-11 19:36:59 +0200

    Bubblesort implemented TODO: autoAnimation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Menu from "./components/menu/Menu";
 import Visualizer from "./components/visualizer/Visualizer";
@@ -7,16 +7,33 @@ import bubbleSort from "./algorithms/bubblesort";
 import debugArray from "./algorithms/debug";
 
 function App() {
-  let arraySize = 80;
+  let arraySize = 40;
 
   const [array, setArray] = useState(generateRandomArray(arraySize, 50, 280));
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (running) {
+        setRunning(true);
+        sortArray();
+      }
+    }, 10);
+  }, [running]);
 
   const resetArray = () => {
     setArray(generateRandomArray(arraySize, 50, 280));
   };
 
+  const run = () => {
+    setRunning(true);
+  };
+
   const sortArray = () => {
     setArray(bubbleSort(array));
+    if (debugArray(array)) {
+      setRunning(false);
+    }
     //DEBUG: check if array is sorted
     console.log(debugArray(array));
   };
@@ -24,7 +41,7 @@ function App() {
   return (
     <div>
       <h1>Algorithms visualizer</h1>
-      <Menu resetArray={() => resetArray()} sortArray={() => sortArray()} />
+      <Menu resetArray={() => resetArray()} run={() => run()} />
       <Visualizer array={array} />
     </div>
   );
--- src/algorithms/bubblesort.js
+++ src/algorithms/bubblesort.js
@@ -1,15 +1,24 @@
+let i = 0;
+let j = 0;
+
 export default function bubbleSort(array) {
   const sortedArray = [...array];
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array.length - 1 - i; j++) {
-      if (sortedArray[j] > sortedArray[j + 1]) {
-        let temp = sortedArray[j];
-        sortedArray[j] = sortedArray[j + 1];
-        sortedArray[j + 1] = temp;
-      }
-    }
+  if (sortedArray[j] > sortedArray[j + 1]) {
+    let temp = sortedArray[j];
+    sortedArray[j] = sortedArray[j + 1];
+    sortedArray[j + 1] = temp;
   }
+  animateBubbleSort(sortedArray);
 
   return sortedArray;
+}
+
+function animateBubbleSort(array) {
+  if (j < array.length - 1 - i) {
+    j++;
+  } else {
+    j = 0;
+    i++;
+  }
 }
