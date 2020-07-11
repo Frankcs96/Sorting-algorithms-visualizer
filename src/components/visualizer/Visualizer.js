7d1e50e020631e149ccd7c3c0aa9d9cfdbbb9ee4
frankcs96 <fcsuarez96@gmail.com>
2020-07-11 07:56:01 +0200

    Cleaning code

--- src/components/visualizer/Visualizer.js
+++ src/components/visualizer/Visualizer.js
@@ -1,22 +1,15 @@
-import React, {useState} from "react";
-import "./visualizer.css"
+import React from "react";
+import "./visualizer.css";
 import Bar from "./Bar";
-import generateRandomArray from "../../utils/arraygenerator";
 
-
-function Visualizer({array}) {
-
-
-
-    return (
-        <div className="visualizer-container">
-            {array.map((element, index) => {
-                return <Bar key={index} value={element}/>
-            })}
-        </div>
-
-    );
+function Visualizer({ array }) {
+  return (
+    <div className="visualizer-container">
+      {array.map((element, index) => {
+        return <Bar key={index} value={element} />;
+      })}
+    </div>
+  );
 }
-
 
 export default Visualizer;
